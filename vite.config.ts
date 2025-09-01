719b2e15434512470bdcc6c46bcc8307661d6b31
tech-example <st661102064120@pcru.ac.th>
2025-09-01 16:11:53 +0700

    Fix config to use SPA mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'; // <-- 1. import เข้ามา
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    react(), // <-- 2. เพิ่ม react() เข้าไปใน list (แนะนำให้อยู่ตัวแรก)
+    tailwindcss(), 
+    reactRouter(), 
+    tsconfigPaths()
+  ],
+  base: '/',
 });
